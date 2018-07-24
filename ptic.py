abc95e204fa4a7a20a3d11617893a0b083b0655e
rymo90 <red.01yassin@gmail.com>
2018-07-24 14:39:06 +0200

    dif 1.4

---
+++ ptic.py
@@ -0,0 +1,20 @@
+s = int(input())
+n = input()
+namn = {
+    "Adrian": "ABC",
+    "Bruno": "BABC",
+    "Goran": "CCAABB",
+}
+points = {}
+
+for lad in namn:
+    lad_pts = 0
+    for ans, guess in zip(n, namn[lad] * s):
+        if ans == guess:
+            lad_pts += 1
+    points[lad] = lad_pts
+best = max(points.values())
+print(best)
+for lad in sorted(points):
+    if points[lad] == best:
+        print(lad)
