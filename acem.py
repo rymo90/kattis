1bd669a6491bb86ece6b94b5ab7d1ce733077e2f
rymo90 <red.01yassin@gmail.com>
2018-08-07 00:44:10 +0200

    dif 1.4

--- acem.py
+++ acem.py
@@ -1,18 +1,19 @@
-import sys
+
 count = 0
 time = 0
 lis = dict()
+lista = []
 while True:
     x = input().split()
     if x[0] == "-1":
-        print(count, time)
         break
-
     else:
+        lista.append(x)
         lis[x[1]] = 0
-        if x[2] == "right":
-            count += 1
-            print(lis)
-            time += int(x[0])+lis[x[1]]*20
-        else:
-            lis[x[1]] += 1
+for i in range(len(lista)):
+    if lista[i][2] == "right":
+        count += 1
+        time += int(lista[i][0])+(lis[lista[i][1]]*20)
+    else:
+        lis[lista[i][1]] += 1
+print(count, time)
