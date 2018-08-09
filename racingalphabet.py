f7ccfdbc2d262b3da84b134f3ca42b44576a5b65
rymo90 <red.01yassin@gmail.com>
2018-08-09 23:20:26 +0200

    dif 1.5

--- racingalphabet.py
+++ racingalphabet.py
@@ -1,22 +1,18 @@
 
 
 import math
-INDEX = {c: i for i, c in enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ '")}
-CIRCUM = math.pi*60
-SPEED = 15
-
-
-def circle_distance(a, b):
-    d = min(abs(INDEX[a]-INDEX[b]),
-            len(INDEX) - abs(INDEX[a]-INDEX[b]))
-    return CIRCUM*(d/len(INDEX))
-
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ '"
+circle = (math.pi*60)
 
 n = int(input())
-
 for _ in range(n):
     s = input()
-    time = len(s)
-    for a, b in zip(s, s[1:]):
-        time += circle_distance(a, b)/SPEED
+    time = len(alpha)
+
+    for i in range(len(s)-1):
+        index = alpha.index(s[i])
+        nextIndex = alpha.index(s[i+1])
+        distance = min(abs(index-nextIndex), len(alpha)-abs(index-nextIndex))
+
+        time += circle*(distance/len(alpha))/15
     print(time)
