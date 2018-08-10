9ddca1c4131a1f1cbff3fa5e9f89247d487547bd
Redwan Mohammedberhan <red.01yassin@gmail.com>
2018-08-10 19:07:19 +0200

    dif 1.5

--- parking.py
+++ parking.py
@@ -1,23 +1,12 @@
-a, b, c = map(int, input().split())
-dic = {}
-dic[1] = a
-dic[2] = b*2
-dic[3] = c*3
-print(dic)
-fst = []
-sec = []
-for _ in range(3):
-    temp1, temp2 = map(int, input().split())
-    fst.append(temp1)
-    sec.append(temp2)
-print(min(fst), max(sec))
-car = 0
-cost = 0
-previous =
-for i in range(min(fst), max(sec)+1):
-    if i in fst:
-        cost += dic[i] * abs(previous-i)
-        previous = i
-        car += 1
-    elif i in sec:
-        print("out", i)
+prices = [0 ,*map(int, input().split())]
+trucks = [tuple(map(int, input().split())) for _ in range(3)]
+start = [t[0] for t in trucks]
+end = [t[1] for t in trucks]
+current_trucks= 0
+price= 0
+for i in range(min(start), max(end)+1):
+    current_trucks += start.count(i)
+    current_trucks -= end.count(i)
+    price+= prices[current_trucks]* current_trucks
+
+print(price)
